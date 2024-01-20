c2ed5b31a22229c83ade6fb6fc95a95dccb3ab1d
SetuBisht <vish.vb11@gmail.com>
2024-01-20 18:25:27 +0530

    cores

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,21 @@ const corsOption = {
   credentials: true,
   origin: [process.env.FRONT_URL],
 };
+
 app.options("*", cors());
 app.use(cors(corsOption));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
 app.use("/storage", express.static("storage"));
 
 const PORT = process.env.PORT || 5500;
